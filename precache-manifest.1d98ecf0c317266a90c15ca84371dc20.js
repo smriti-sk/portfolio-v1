199d7c8f76f2e7aa5f32e505b0c9c9f5883032d3
smriti-sk <ksmritie@gmail.com>
2023-05-26 13:59:21 +0530

    Updates

---
+++ precache-manifest.1d98ecf0c317266a90c15ca84371dc20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "675f7bc5a23d859b7046",
+    "url": "/smritikumari/static/css/main.bfd2036f.chunk.css"
+  },
+  {
+    "revision": "675f7bc5a23d859b7046",
+    "url": "/smritikumari/static/js/main.675f7bc5.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/smritikumari/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "bf4200ca0cf620915e41",
+    "url": "/smritikumari/static/js/2.bf4200ca.chunk.js"
+  },
+  {
+    "revision": "1e152573f68a5b05be51",
+    "url": "/smritikumari/static/js/runtime~main.1e152573.js"
+  },
+  {
+    "revision": "7a30ca8f55464622703b0a820f30ce0b",
+    "url": "/smritikumari/static/media/smriti.7a30ca8f.png"
+  },
+  {
+    "revision": "210d0ac1c4df55d2da6dcc938759c717",
+    "url": "/smritikumari/index.html"
+  }
+];
