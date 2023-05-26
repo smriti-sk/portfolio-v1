843713c9a106bda6e5dc290e28af6f3f7bdd8062
smriti-sk <ksmritie@gmail.com>
2023-05-26 23:49:29 +0530

    home mobile-view corrected

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,24 +7,26 @@ const Header = () => {
     <>
       <section className="main-content">
         <div className="container">
-          <div className="header">
-            <div className="grid-container">
-              <div className="item1">
-                <h2>Smriti&nbsp;Kumari</h2>
-              </div>
-              <div className="item2">
-                <img src={profilePhoto} alt="profile photo" id="profilePhoto" />
-              </div>
-              <div className="item3">A bit about me</div>
-              <div className="item4">
-                Full Stack Developer with a passion for building innovative and
-                user-friendly web applications. Skilled in a variety of front-end
-                and back- end technologies, including HTML, CSS, JavaScript,
-                React.js, Bootstrap, Node.js, API development, SQL, and Database
-                management.
-              </div>
-              <div className="item5">blank</div>
-              <div className="item6">
+          <div className="grid-container">
+            <div className="item1">
+              <h2>Smriti&nbsp;Kumari</h2>
+            </div>
+            <div className="item2">
+              <img src={profilePhoto} alt="profile photo" id="profilePhoto" />
+            </div>
+
+            <div className="item3">A bit about me</div>
+
+            <div className="item4">
+              Full Stack Developer with a passion for building innovative and
+              user-friendly web applications. Skilled in a variety of front-end
+              and back- end technologies, including HTML, CSS, JavaScript,
+              React.js, Bootstrap, Node.js, API development, SQL, and Database
+              management.
+            </div>
+            <div className="item5">blank</div>
+            <div className="item6">
+              <div className="cta-container">
                 <div className="cta cta-1">Resume</div>
                 <div className="cta cta-2">About me</div>
                 <div className="cta cta-3">My Skills</div>
