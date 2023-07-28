97de973382a4821cd538faa5e8a491db8f5f8ad6
smriti-sk <ksmritie@gmail.com>
2023-07-28 14:05:38 +0530

    Updates

---
+++ precache-manifest.1cb6761fbbe40636695c5e58e0cb35b5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0209baec38a34b87926",
+    "url": "/portfolio-v1/static/css/main.5979954f.chunk.css"
+  },
+  {
+    "revision": "d0209baec38a34b87926",
+    "url": "/portfolio-v1/static/js/main.cbe5d644.chunk.js"
+  },
+  {
+    "revision": "3a781d6e3d992edc98c7",
+    "url": "/portfolio-v1/static/js/runtime~main.65234dc5.js"
+  },
+  {
+    "revision": "eefd67cddc3e1f2d6625",
+    "url": "/portfolio-v1/static/js/2.98fab485.chunk.js"
+  },
+  {
+    "revision": "05ab368e0d3f79e1b84a",
+    "url": "/portfolio-v1/static/js/3.31456830.chunk.js"
+  },
+  {
+    "revision": "7a30ca8f55464622703b0a820f30ce0b",
+    "url": "/portfolio-v1/static/media/smriti.7a30ca8f.png"
+  },
+  {
+    "revision": "10a5ab7293bc0c79fa4bef2b73acd7f0",
+    "url": "/portfolio-v1/static/media/SKlblacknobg.10a5ab72.png"
+  },
+  {
+    "revision": "a7dc58fb0f652dce2be14f6729ce5044",
+    "url": "/portfolio-v1/static/media/smritiphoto.a7dc58fb.jpg"
+  },
+  {
+    "revision": "a8244cdeef7f06eea0c94c1d331e4365",
+    "url": "/portfolio-v1/static/media/tictactoe.a8244cde.png"
+  },
+  {
+    "revision": "2752a70d19bec2b544cfe7a9f28b61e9",
+    "url": "/portfolio-v1/static/media/ExpenseTracker.2752a70d.PNG"
+  },
+  {
+    "revision": "ab0c936e14476c939f7fec3ab30d2147",
+    "url": "/portfolio-v1/static/media/portfolio.ab0c936e.PNG"
+  },
+  {
+    "revision": "99facc78bc60b70344b8c750babeccb9",
+    "url": "/portfolio-v1/static/media/VideoConfrencing.99facc78.PNG"
+  },
+  {
+    "revision": "28d63d0ef0fd800faa0f954dc26f510a",
+    "url": "/portfolio-v1/static/media/youtubeclone.28d63d0e.PNG"
+  },
+  {
+    "revision": "b278b2dec7a466b3c0c395d8dc35605a",
+    "url": "/portfolio-v1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/smritikumari/precache-manifest.7480f55e6a35391f3f4c9f1a2051d598.js"
+  "/portfolio-v1/precache-manifest.1cb6761fbbe40636695c5e58e0cb35b5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/smritikumari/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio-v1/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
